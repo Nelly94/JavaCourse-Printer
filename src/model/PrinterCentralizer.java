a8d99ee880bcb8cf7c0fa0dd61947fc85d58f0bd
nellyiriberri <nellyiriberri@gmail.com>
2020-05-27 15:23:05 +0200

    Clean code

--- src/model/PrinterCentralizer.java
+++ src/model/PrinterCentralizer.java
@@ -9,6 +9,10 @@ public class PrinterCentralizer {
     public List<Printer> printers = new ArrayList<>();
     private int index = 0;
 
+    private PrinterCentralizer(){
+        addPrinter(new Printer(0, "Nvidia"));
+    }
+
     public static PrinterCentralizer getInstance(){
         if(instance == null){
             instance = new PrinterCentralizer();
